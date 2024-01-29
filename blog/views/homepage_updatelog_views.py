0efe509cbdfb201d4fbe682124d3788c2fe3a54c
believeriver <nobuyuki.galois@gmail.com>
2024-01-30 05:33:07 +0900

    20240130_01 homepate_updatelog_vies.pyのバグを修正

--- blog/views/homepage_updatelog_views.py
+++ blog/views/homepage_updatelog_views.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.dirname(
 from blog.models import HomePageUpdateModel
 
 
-class HomeIndexView(LoginRequiredMixin, generic.ListView):
+class HomeIndexView(generic.ListView):
     template_name = 'pages/main_home.html'
     model = HomePageUpdateModel
     paginate_by = 10
