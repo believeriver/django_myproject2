172b9bd4573aec536ff0b16d9d07f0e77cab57a4
believeriver <nobuyuki.galois@gmail.com>
2024-01-31 10:16:49 +0900

    20240131_02 OWNER_NAME指定を追加

--- blog/views/account_views.py
+++ blog/views/account_views.py
@@ -9,6 +9,9 @@ sys.path.append(os.path.dirname(os.path.dirname(
 from blog.models import Profile
 
 
+OWNER_NAME = 'nono'
+
+
 class ProfileIndexView(generic.View):
     template_name = 'pages/main_about_me.html'
     # model = Profile
@@ -16,7 +19,7 @@ class ProfileIndexView(generic.View):
     def get(self, request, *args, **kwargs):
         # ユーザー名に基づいてプロフィールオブジェクトを取得
         # username = kwargs.get('username')
-        username = 'tagawanobuyuki'
+        username = OWNER_NAME
         profile = Profile.objects.get(user__username=username)
 
         # テンプレートにデータを渡す
